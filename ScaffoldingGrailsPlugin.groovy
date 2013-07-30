ab0431f2bf6c9775a323eeb4f925b4ca7be91323
Graeme Rocher <graeme.rocher@gmail.com>
2013-07-30 09:39:52 +0200

    update to 2.0.0.RC1

--- ScaffoldingGrailsPlugin.groovy
+++ ScaffoldingGrailsPlugin.groovy
@@ -37,7 +37,7 @@ class ScaffoldingGrailsPlugin {
 
 	private Logger log = LoggerFactory.getLogger(getClass())
 
-	String version = '2.0.0.BUILD-SNAPSHOT'
+	String version = '2.0.0.RC1'
 	String grailsVersion = '2.3 > *'
 	def pluginExcludes = ['grails-app/domain/**']
 
