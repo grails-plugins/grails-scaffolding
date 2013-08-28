43ec558eb226bd007bc022cc7d8afd9c5ce30a6f
Burt Beckwith <burt@burtbeckwith.com>
2013-08-28 02:12:57 -0400

    GPSCAFFOLD-61 added null checks, made html and rest responses consistent

--- src/templates/scaffolding/AsyncController.groovy
+++ src/templates/scaffolding/AsyncController.groovy
@@ -29,18 +29,23 @@ class ${className}Controller {
 
     def save(${className} ${propertyName}) {
         ${className}.async.withTransaction {
+            if (${propertyName} == null) {
+                notFound()
+                return
+            }
+
             if(${propertyName}.hasErrors()) {
                 respond ${propertyName}.errors, view:'create' // STATUS CODE 422
+                return
             }
-            else {
-                ${propertyName}.save flush:true
-                request.withFormat {
-                    form {
-                        flash.message = message(code: 'default.created.message', args: [message(code: '${propertyName}.label', default: '${className}'), ${propertyName}.id])
-                        redirect ${propertyName}
-                    }
-                    '*' { respond ${propertyName}, [status: CREATED] }
+
+            ${propertyName}.save flush:true
+            request.withFormat {
+                form {
+                    flash.message = message(code: 'default.created.message', args: [message(code: '${propertyName}.label', default: '${className}'), ${propertyName}.id])
+                    redirect ${propertyName}
                 }
+                '*' { respond ${propertyName}, [status: CREATED] }
             }
         }
     }
@@ -54,22 +59,23 @@ class ${className}Controller {
     def update(Long id) {
         ${className}.async.withTransaction {
             def ${propertyName} = ${className}.get(id)
-            if(${propertyName} == null) {
-                render status: NOT_FOUND
+            if (${propertyName} == null) {
+                notFound()
                 return
             }
+
             ${propertyName}.properties = params
             if( !${propertyName}.save(flush:true) ) {
                 respond ${propertyName}.errors, view:'edit' // STATUS CODE 422
+                return
             }
-            else {
-                request.withFormat {
-                    form {
-                        flash.message = message(code: 'default.updated.message', args: [message(code: '${className}.label', default: '${className}'), ${propertyName}.id])
-                        redirect ${propertyName}
-                    }
-                    '*'{ respond ${propertyName}, [status: OK] }
+
+            request.withFormat {
+                form {
+                    flash.message = message(code: 'default.updated.message', args: [message(code: '${className}.label', default: '${className}'), ${propertyName}.id])
+                    redirect ${propertyName}
                 }
+                '*'{ respond ${propertyName}, [status: OK] }
             }
         }
     }
@@ -77,19 +83,30 @@ class ${className}Controller {
     def delete(Long id) {
         ${className}.async.withTransaction {
             def ${propertyName} = ${className}.get(id)
-            if(${propertyName}) {
-                ${propertyName}.delete flush:true
-                request.withFormat {
-                    form {
-                        flash.message = message(code: 'default.deleted.message', args: [message(code: '${className}.label', default: '${className}'), ${propertyName}.id])
-                        redirect action:"index", method:"GET"
-                    }
-                    '*'{ render status: NO_CONTENT }
+            if (${propertyName} == null) {
+                notFound()
+                return
+            }
+
+            ${propertyName}.delete flush:true
+
+            request.withFormat {
+                form {
+                    flash.message = message(code: 'default.deleted.message', args: [message(code: '${className}.label', default: '${className}'), ${propertyName}.id])
+                    redirect action:"index", method:"GET"
                 }
+                '*'{ render status: NO_CONTENT }
             }
-            else {
-                render status: NOT_FOUND
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: '${propertyName}.label', default: '${className}'), params.id])
+                redirect action: "index", method: "GET"
             }
+            '*'{ render status: NOT_FOUND }
         }
     }
 }
