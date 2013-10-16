20f8db86a6c2dbe4dd7bc759ddfd12eeeba36ac9
Graeme Rocher <graeme.rocher@gmail.com>
2013-10-16 09:38:32 +0200

    fix for GRAILS-10596 "Spock tests always fail (404/302)"

--- src/templates/scaffolding/Spec.groovy
+++ src/templates/scaffolding/Spec.groovy
@@ -92,7 +92,9 @@ class ${className}ControllerSpec extends Specification {
             controller.update(null)
 
         then:"A 404 error is returned"
-            status == 404
+            response.redirectedUrl == '/${propertyName}/index'
+            flash.message != null
+
 
         when:"An invalid domain instance is passed to the update action"
             response.reset()
@@ -120,7 +122,8 @@ class ${className}ControllerSpec extends Specification {
             controller.delete(null)
 
         then:"A 404 is returned"
-            status == 404
+            response.redirectedUrl == '/${propertyName}/index'
+            flash.message != null
 
         when:"A domain instance is created"
             response.reset()
